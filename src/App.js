37106b5c25666c854f557406c1c89d39e813cdf5
Shannon TJ <shannontj@shaw.ca>
2020-12-02 15:17:12 -0700

    hackernews api setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getStoryIds } from "./services/hnApi";
 
 export const App = () => {
-  return <p>hello</p>;
+  const [storyIds, setStoryIds] = useState([]);
+
+  useEffect(() => {
+    getStoryIds().then((data) => setStoryIds(data));
+  }, []);
+  return <p>{storyIds}</p>;
 };
