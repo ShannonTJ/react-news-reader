3fc925a2732a1521fd36d5f1a51a13fd0d399100
Shannon TJ <shannontj@shaw.ca>
2020-12-03 16:25:09 -0700

    story container styled components

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { getStoryIds } from "../services/hnApi";
 import { Story } from "../components/Story";
+import {
+  GlobalStyle,
+  StoriesContainerWrapper,
+} from "../styles/StoriesContainerStyles";
 
 export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
@@ -12,10 +16,13 @@ export const StoriesContainer = () => {
   //loop over the stories
   return (
     <>
-      <h1>Hacker News Stories</h1>
-      {storyIds.map((storyId) => (
-        <Story key={storyId} storyId={storyId} />
-      ))}
+      <GlobalStyle></GlobalStyle>
+      <StoriesContainerWrapper data-test-id="stories-container">
+        <h1>Hacker News Stories</h1>
+        {storyIds.map((storyId) => (
+          <Story key={storyId} storyId={storyId} />
+        ))}
+      </StoriesContainerWrapper>
     </>
   );
 };
